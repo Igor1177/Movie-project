a96d9e317516caeaeee51d231c0dbaddad98510e
Igor <95220593+Igor1177@users.noreply.github.com>
2022-12-02 17:56:55 +0400

    перерисовал машину

--- main.cpp
+++ main.cpp
@@ -19,18 +19,6 @@ void DrawFon()
     txSetColor(RGB(195, 195, 195));
     txRectangle(0,600,1450, 780);
 
-    //razmetka next
-//    txSetFillColor(TX_BLACK);
-//    txSetColor(TX_BLACK);
-//    txRectangle(55,640,165, 700);
-//    txRectangle(55+195,640,165+195, 700);
-//    txRectangle(250+195,640,360+195, 700);
-//    txRectangle(445+195,640,555+195, 700);
-//    txRectangle(640+195,640,750+195, 700);
-//    txRectangle(835+195,640,945+195, 700);
-//    txRectangle(1030+195,640,1140+195, 700);
-//    txRectangle(1225+195,640,1335+195, 700);
-
     txSetColor (RGB(34, 177, 76));
     txSetFillColor (RGB(34, 177, 76));
     txRectangle(0, 400, 1450, 600);
@@ -274,28 +262,29 @@ void DrawCar(int x)
     //тело
     txSetColor(TX_BLACK, 3);
     txSetFillColor(RGB(125, 249, 255));
-    POINT Car[17] = {{x+200, 685}, {x+185, 660}, {x+140, 660}, {x+130, 685}, {x+85, 685}, {x+85, 640},
-    {x+95, 640}, {x+95, 615}, {x+165, 570}, {x+460, 570}, {x+500, 615}, {x+605, 615},
-    {x+575, 685}, {x+515, 685}, {x+495, 660}, {x+455, 660}, {x+445, 685}};
-    txPolygon (Car, 17);
+    POINT Car[8] = {{x+190, 610}, {x+345, 610},
+    {x+345, 515}, {x+580, 515},
+    {x+600, 605}, {x+650, 605},
+    {x+650, 710}, {x+190, 710}};
+    txPolygon (Car, 8);
 
     //окна
-    txSetColor(RGB(63, 72, 204));
+    txSetColor(TX_NULL);
     txSetFillColor(RGB(63, 72, 204));
-    POINT CarWindow[4] = {{x+330, 575}, {x+330, 615}, {x+495, 615}, {x+455, 575}};
+    POINT CarWindow[4] = {{x+480, 517}, {x+480, 605}, {x+599, 605}, {x+579, 517}};
     txPolygon (CarWindow, 4);
 
     //фары
     txSetColor(TX_YELLOW);
     txSetFillColor(TX_YELLOW);
-    POINT CarHeadLight[4] = {{x+605, 615}, {x+590, 645}, {x+545, 645}, {x+545, 615}};
+    POINT CarHeadLight[4] = {{x+625, 607}, {x+625, 650}, {x+648, 650}, {x+648, 607}};
     txPolygon (CarHeadLight, 4);
 
     //колеса
     txSetColor(TX_BLACK);
     txSetFillColor(TX_BLACK);
-    txCircle(x+165, 695, 35);
-    txCircle(x+478, 695, 35);
+    txCircle(x+225, 735, 35);
+    txCircle(x+620, 735, 35);
 }
 
 
